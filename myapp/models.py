fb2d852356947e170a89166af6914db7896991ac
hyojin <ahj2079@likelion.com>
2019-07-30 22:26:36 +0900

    배포하자

--- myapp/models.py
+++ myapp/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
+    author = models.ForeignKey(User, on_delete = models.CASCADE, null = True)
     title =  models.CharField(max_length=100)
     pub_date=models.DateTimeField('date published')
-    image = models.ImageField(upload_to = 'images/', null = True)
+    image = models.ImageField(upload_to = 'images/', null = True, blank = True)
     body = models.TextField(max_length= 500 )
    
-
     def __str__(self):
         return self.title
 
